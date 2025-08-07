ca24378a829a57e409e478f2baca0ee865c8b059
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-08-08 02:05:58 +0930

    java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,3 +28,11 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
